ea01af55bc88f1e70b14b2e8521948576eb24516
tingxxu <friendship-119@163.com>
2016-08-29 19:07:04 +0800

    init project

---
+++ Assets/Script/DevIotManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+
+public class DevIotManager : MonoBehaviour {
+
+	// Use this for initialization
+
+	bool isLoadingConfig = false;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		GetThingsConfig ();
+	}
+
+	void GetThingsConfig(){
+		if (isLoadingConfig == false) {
+			isLoadingConfig = true;
+			WebClient client = new WebClient ();
+			client.DownloadStringCompleted += (object sender, DownloadStringCompletedEventArgs e) => {
+				if (e.Error == null) {
+					print (e.Result);
+
+				} else {
+					isLoadingConfig = false;
+				}
+			};
+			client.DownloadStringAsync (new System.Uri("http://www.baidu.com"));
+		}
+
+	}
+}
