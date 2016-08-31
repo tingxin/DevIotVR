c54a2c6c8f13a7f80dda28473158d05fb3cda31a
tingxxu <friendship-119@163.com>
2016-08-31 18:58:41 +0800

    update

--- Assets/Script/DevIotManager.cs
+++ Assets/Script/DevIotManager.cs
@@ -1,20 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Net;
+using DevIotResource;
+using Newtonsoft.Json;
+using System;
 
 public class DevIotManager : MonoBehaviour {
 
 	// Use this for initialization
 
+	public string ServerAddress = "10.140.92.25:9000";
+
 	bool isLoadingConfig = false;
+	bool thingsLoaded = false;
+
+	WorkSpace workspace;
+
+	string testData = "{\"things\":{\"devices\":[{\"id\":\"MQ2\",\"name\":\"Gas & Smoke-01\",\"kind\":\"gas\",\"gateway\":\"GatewayTools-mock\",\"description\":\"\",\"icon\":\"/images/icons/MQ2.png\",\"active\":true,\"properties\":[{\"name\":\"value\",\"type\":0.0,\"required\":false,\"value\":300.0,\"description\":\"getValue\"}],\"actions\":[],\"settings\":[{\"name\":\"Base\",\"type\":0.0,\"description\":\"setBase\"},{\"name\":\"Step\",\"type\":0.0,\"description\":\"setStep\"}],\"location\":{\"latitude\":40.7324,\"longitude\":-73.8248079}}]}}";
+	string testDevice="{\"id\":\"MQ2\",\"name\":\"Gas & Smoke-01\",\"kind\":\"gas\",\"gateway\":\"GatewayTools-mock\",\"description\":\"\",\"icon\":\"/images/icons/MQ2.png\",\"active\":true,\"properties\":[{\"name\":\"value\",\"type\":0.0,\"required\":false,\"value\":300.0,\"description\":\"getValue\"}],\"actions\":[],\"settings\":[{\"name\":\"Base\",\"type\":0.0,\"description\":\"setBase\"},{\"name\":\"Step\",\"type\":0.0,\"description\":\"setStep\"}],\"location\":{\"latitude\":40.7324,\"longitude\":-73.8248079}}";
+	string testProperty="{\"name\":\"value\",\"type\":0.0,\"required\":false,\"value\":300.0,\"description\":\"getValue\"}";
 	void Start () {
-	
+		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		GetThingsConfig ();
+
+		LoadThings ();
 	}
 
 	void GetThingsConfig(){
@@ -25,12 +40,36 @@ public class DevIotManager : MonoBehaviour {
 				if (e.Error == null) {
 					print (e.Result);
 
+					try{
+						
+						this.workspace = JsonConvert.DeserializeObject<WorkSpace>(e.Result);
+						thingsLoaded = true;
+					}
+					catch(Exception ex){
+						print("Json format error: when format the WorkSpace data" +ex.Message);
+						thingsLoaded = false;
+					}
+						
 				} else {
 					isLoadingConfig = false;
+					print(e.Error.Message);
 				}
 			};
-			client.DownloadStringAsync (new System.Uri("http://www.baidu.com"));
+			string requestApi = string.Format ("http://{0}/api/v2/registries", ServerAddress);
+			client.DownloadStringAsync (new System.Uri(requestApi));
 		}
 
+	}
+
+	void LoadThings(){
+		if (thingsLoaded) {
+			print (this.workspace.things.devices.Count.ToString());
+			if (this.workspace.things.devices.Count > 0) {
+				for (int i = 0; i < this.workspace.things.devices.Count; i++) {
+					Device device = this.workspace.things.devices [i];
+
+				}
+			}
+		}
 	}
 }
---
+++ Assets/Script/WorkSpace.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DevIotResource{
+
+	public class WorkSpace {
+
+		public Things things;
+	}
+
+
+	public class Things{
+		public List<Device> devices;
+	}
+
+	public class Tools{
+		//TODO:
+	}
+
+	public class Device{
+		public string id;
+		public string name;
+		public string kind;
+		public string gateway;
+		public string description;
+		public string icon;
+		public bool active;
+
+		public List<Property> properties;
+		public List<Setting> settings;
+		public List<Action> actions;
+
+		public Location location;
+	}
+
+
+	public class Property{
+		public string name;
+		public double type;
+		public bool required;
+		public object value;
+		public string description;
+	}
+
+	public class Flog{
+
+		public string name;
+		public string kind;
+		public bool enabled;
+		public string link;
+		public string description;
+		public string connector;
+		public string icon;
+	}
+
+	public class Tool{
+		//TODO:
+	}
+
+	public class Setting{
+
+		public string name;
+		public string description;
+		public float type;
+
+	}
+
+	public class Action{
+
+		public string name;
+		public string description;
+		public string id;
+
+		public List<Parameter> parameters;
+	}
+
+	public class Parameter{
+		public string id;
+		public string name;
+		public double type;
+		public bool required;
+		public object value;
+		public string description;
+	}
+
+	public class Location{
+		public double latitude;
+		public double longitude;
+	}
+}
